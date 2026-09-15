fe54a210c13988b2df109ff513f40962afd5a7ed
KimSummmmer <mi.amor.ban@gmail.com>
2026-09-15 15:49:08 +0900

    v.9.15.3
    - requirements fixed

--- cocktails/app.py
+++ cocktails/app.py
@@ -184,4 +184,5 @@ def recommend_by_cocktail():
 
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
